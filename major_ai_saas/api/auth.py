f3911ca7696f4e34c3072ea055466ddc4367d47a
Rushikesh-hub <rushikeshmuneshwar2@gmail.com>
2026-02-09 18:46:29 +0530

    Day-23: User database, password hashing, and JWT authentication

--- major_ai_saas/api/auth.py
+++ major_ai_saas/api/auth.py
@@ -26,3 +26,20 @@ def signup(user: schemas.UserCreate, db:Session = Depends(get_db)):
         email=user.email,
         hashed_password = hash_password(user.password)
     )
+
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return {"message" : "User created successfully"}
+
+@router.post("/login", response_model=schemas.Token)
+def login(user: schemas.UserLogin, db:Session = Depends(get_db)):
+    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+
+    if not db_user or not verify_password(user.password, db_user.hashed_password):
+        raise HTTPException(status_code=401,detail="Invalid credentials")
+    
+    token = create_access_token({"sub": db_user.email})
+
+    return {"access_token": token, "token_type": "bearer"}
--- major_ai_saas/api/main.py
+++ major_ai_saas/api/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
-
+from api.auth import router as auth_router
 
 app = FastAPI(title="AI Hiring Assistant")
+
+app.include_router(auth_router)
+
 
 @app.get("/")
 def root():
